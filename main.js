202f8dc47431df63e4a0baf1cdd8bab5da312361
LouayLammouchi <lammouchilouay@gmail.com>
2021-10-15 16:38:00 +0100

    comments

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+//this is our function who generate the rgb randomly the output will be in this for (11,11,11)
 function generate_rgb(){
     return '('+ Math.floor(Math.random()*266)+
      "," + Math.floor(Math.random()*266)+" ," +
@@ -5,12 +7,14 @@ function generate_rgb(){
 
 } 
  
-
+// here we created the array who will contain the different rgb for the 6 numbers
   var arrRGB = [] ;
+  //the resetbutton will empty the array 
 $("#reset").click(function(){
      arrRGB = [] 
     
   })
+// after the reset every color will be regenerated automatically and his value will be added in the array
 $("#reset").click(function(){
     var guess1 = generate_rgb() ;
     arrRGB.push(guess1)
@@ -45,20 +49,21 @@ $("#reset").click(function(){
     $("#color6").css("color","rgb"+guess6)
   })
   var score = 0 ;
-  var s7i7;
- var reb7 = false ;
- var dgueza ;
+  var s7i7; //
+ var reb7 = false ;// if you win or no
+ var dgueza ; // the choice of the player
+ // the submitt will take the value
  $("#submitt").click(function(){
   dgueza = document.getElementById("dagueza").value
 })
-
+//just to see the array
  $("#reset").click(function(){
   console.log(arrRGB)
   })
-
+//here we will take a rgb value randomly from the array , it will be the correct answer ;
   $("#reset").click(function(){
     s7i7= Math.floor(Math.random()*6) ;
-    console.log(s7i7+1) 
+    console.log(s7i7+1) // because we wanna see the correct answer we added 1 th choices start from 1 and the index of the array from 0
      
 
    
@@ -67,7 +72,7 @@ $("#reset").click(function(){
   
     }) 
     
-
+/// now we campare the choice and the correct answer and see if he won or not
     $("#submitt").click(function(){
       if (dgueza==s7i7+1) {
         reb7 = true
